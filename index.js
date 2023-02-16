49e5b8dfaa303b1f6cd5fc01e1e9b447fb1e8775
Nate J <nathan.evan.johnson@gmail.com>
2023-02-16 02:15:27 -0800

    Started initial structure and included node packages to use

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+// TODO: Include packages needed for this application
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// TODO: Create an array of questions for user input
+const questions = [
+
+    type:
+    message:
+    name:
+    default:
+    validate:
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
